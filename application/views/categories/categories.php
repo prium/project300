81bfdf311042f52db2aa2c618a86728dc6132109
Prium <prium47@gmail.com>
2013-10-21 13:21:19 +0600

    responsive fixed for categories

--- application/views/categories/categories.php
+++ application/views/categories/categories.php
@@ -2,58 +2,61 @@
 
 	<div class="span6">
 
-		<form class="form-horizontal">
-			<div class="portlet box blue">
-				<div class="portlet-title">
-					<div class="caption"><i class="icon-reorder"></i>Add New Category</div>
-					<div class="tools">
-						<a href="javascript:;" class="collapse"></a>
-					</div>
-				</div>
-				<div class="portlet-body form">
-					<div class="alert alert-error hide">
-						<button class="close" data-dismiss="alert"></button>
-						You have some form errors. Please check below.
-					</div>
-					<div class="alert alert-success hide">
-						<button class="close" data-dismiss="alert"></button>
-						Your form validation is successful!
-					</div>
-
-					<div class="control-group">
-						<label class="control-label">Name</label>
-						<div class="controls">
-							<input type="text" placeholder="Name" class="m-wrap large">
+		<!-- BEGIN ADD CATEGORIES FORM-->
+		<div class="row-fluid">
+			<div class="span12">
+				
+				<!-- BEGIN FORM-->
+				<form action="<?php echo site_url('projects/process-new'); ?>" id="requiredForm" class="form-horizontal" novalidate="novalidate" method="post">
+					<?php $this->load->view('includes/notify'); ?>
+					<div class="portlet box blue">
+						<div class="portlet-title">
+							<div class="caption"><i class="icon-reorder"></i>Basic Information</div>
+							<div class="tools">
+								<a href="javascript:;" class="collapse"></a>
+							</div>
 						</div>
-					</div><!--/control-group-->
-
-					<div class="control-group">
-						<label class="control-label">Slug</label>
-						<div class="controls">
-							<input type="text" placeholder="Slug" class="m-wrap large">
+						<div class="portlet-body form">
+							<div class="alert alert-error hide">
+								<button class="close" data-dismiss="alert"></button>
+								You have some form errors. Please check below.
+							</div>
+							<div class="alert alert-success hide">
+								<button class="close" data-dismiss="alert"></button>
+								Your form validation is successful!
+							</div>
+							<div class="control-group">
+								<label class="control-label">Name<span class="required">*</span></label>
+								<div class="controls">
+									<input type="text" name="projectName" id="projectName" data-required="1" class="span6 m-wrap">
+								</div>
+							</div>
+							<div class="control-group">
+								<label class="control-label">Slug<span class="required">*</span></label>
+								<div class="controls">
+									<input name="projectSlug" id="projectSlug" type="text" class="span6 m-wrap">
+								</div>
+							</div>
+							<div class="control-group">
+								<label class="control-label">Parent<span class="required">*</span></label>
+								<div class="controls">
+									<?php echo form_dropdown('projectStatus', $categories, '', 'class="span6 m-wrap"'); ?>
+								</div>
+							</div>
+							
 						</div>
-					</div><!--/control-group-->
-
-					<div class="control-group">
-						<label class="control-label">Parent</label>
-						<div class="controls">
-							<select class="large m-wrap" tabindex="1">
-								<option value="Category 1">None</option>
-								<option value="Category 1">Category 2</option>
-								<option value="Category 1">Category 3</option>
-							</select>
-						</div>
-					</div><!--/control-group-->
-
+					</div><!--/portlet box grey-->
+					
 					<div class="form-actions">
-						<button type="submit" class="btn blue">Save</button>
-						<button type="button" class="btn">Cancel</button>
-					</div><!--/form-actions-->
+						<button type="submit" class="btn green">Submit</button>
+						<input type="reset" class="btn" value="Cancel">
+					</div>
+				</form>
+				<!-- END FORM-->
 
-				</div>
-			</div>
-
-	</form>
+			</div><!--/span12-->
+		</div>
+		<!-- END ADD CATEGORIES FORM-->
 
 
 	</div><!--/span6-->
